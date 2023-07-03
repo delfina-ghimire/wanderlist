cb17e7ed081cf45ca3c6b62c72923b43a7d7cbe1
delfina-ghimire <delfina.ghimire@gmail.com>
2023-07-03 15:35:32 +0545

    feat: Add form

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import Logo from "./components/Logo"
 import Form from "./components/Form";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
-
+import { useState } from "react";
 
 function App() {
-  
+  const [items, setItems] = useState([]);
+
+  function handleItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
-   <>
-   <Logo/>
-   <Form/>
-   <PackingList/>
-   <Stats/>
-   
-   </>
-  )
+    <>
+      <Logo />
+      <Form onAddItems={handleItems} />
+      <PackingList item={items} />
+      <Stats />
+    </>
+  );
 }
 
 export default App
--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,19 +1,22 @@
-import Item from "./Item";
-
-
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-];
-
-export default function PackingList() {
+export default function PackingList({ item }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {item.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
   );
 }
