bb18108deb4afc78caaa20fb7768f888556ad143
delfina-ghimire <delfina.ghimire@gmail.com>
2023-07-05 13:22:02 +0545

    feat : Toggle packed / unpacked

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,27 @@ function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDelete(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <>
       <Logo />
       <Form onAddItems={handleItems} />
-      <PackingList item={items} />
+      <PackingList
+        item={items}
+        onDeleteItem={handleDelete}
+        onToggleItem={handleToggleItem}
+      />
       <Stats />
     </>
   );
--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,22 +1,34 @@
-export default function PackingList({ item }) {
+export default function PackingList({ item, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {item.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => {
+          onToggleItem(item.id);
+        }}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
