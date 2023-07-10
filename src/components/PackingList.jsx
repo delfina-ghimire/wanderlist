04c91aea9d57e54716cd0d572a72c165a73f596e
delfina-ghimire <delfina.ghimire@gmail.com>
2023-07-10 15:46:53 +0545

    feat: sort list

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 export default function PackingList({ item, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = item;
+
+  if (sortBy === "description")
+    sortedItems = item
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = item
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {item.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -11,6 +28,13 @@ export default function PackingList({ item, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="action">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort By Input Order</option>
+          <option value="description">Sort By Description</option>
+          <option value="packed">Sort By Packed Status</option>
+        </select>
+      </div>
     </div>
   );
 }
